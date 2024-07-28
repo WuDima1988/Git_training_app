20cf0e1c84373f51d9bc5876710978a6b3dab99d
wudima1988 <wudima1988@gmail.com>
2024-07-28 11:59:00 +0300

    Made lesson2

---
+++ src/main/java/com/wudima/lesson2/Task2.java
@@ -0,0 +1,15 @@
+package com.wudima.lesson2;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        task(2);
+    }
+
+    private static void task(int i){
+
+        System.out.println("hello "+i);
+    }
+
+}
